73b2c1bc57a20570b37eedd5298b38fc0baf8803
Vedant Vrattikoppa <vedantv355@gmail.com>
2022-04-23 12:34:14 +0530

    change dimensions so the maze fits on the screen

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Author: Vedant Vrattikoppa
 
 const len = 50, breadth = 50; // number of tiles in maze
-const cellWidth = 8, borderWidth = 2;
+const cellWidth = 7, borderWidth = 1;
 const width = len * (cellWidth + borderWidth) + borderWidth;
 const height = breadth * (cellWidth + borderWidth) + borderWidth;
 
