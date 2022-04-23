dd0d46b7c3c1fb9e719ca3b3977968c68fc8c5f1
Vedant Vrattikoppa <vedantv355@gmail.com>
2022-04-23 13:17:05 +0530

    update dimensions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Author: Vedant Vrattikoppa
 
 const len = 50, breadth = 50; // number of tiles in maze
-const cellWidth = 10, borderWidth = 5;
+const cellWidth = 8, borderWidth = 4;
 const width = len * (cellWidth + borderWidth) + borderWidth;
 const height = breadth * (cellWidth + borderWidth) + borderWidth;
 
